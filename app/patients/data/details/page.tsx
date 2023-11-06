446a53301be924824dfb9810a9ea98c3494b9cab
Ebube Life <ebubeemeka19@gmail.com>
2023-11-06 15:39:41 +0100

    ...

--- app/patients/data/details/page.tsx
+++ app/patients/data/details/page.tsx
@@ -104,7 +104,17 @@ useEffect(() => {
  </div>
 </div>
 
+<div className="flex w-screen justify-center px-10">
 
+<div className='w-1/3 bg-slate-100 py-2 px-6 flex mt-8 rounded-md justify-center '>
+
+<p className="text-md text-center font-medium text-zinc-800"> I am a patient</p>
+
+ 
+
+  
+</div>
+</div>
 <div className='w-screen 0 md:pb-10 md:pl-20 md:pr-20 md:pt-10 flex justify-center'>
     
   {/* Create 6 copies of inner-img */}
