29a0c1a9f0970e32f55e5d55d3e055e7a55acc04
Ebube Life <ebubeemeka19@gmail.com>
2023-11-06 14:38:40 +0100

    ....

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,10 +10,42 @@ export default function Footer() {
         <div className='h-32'></div>
 
 
-<div className="w-screen h-[300px] bg-slate-200 mt-auto grid place-content-center text-3xl text-slate-300">
+<div className="w-screen h-[300px] bg-slate-200 mt-auto grid place-content-center text-3xl md:px-10 grid-cols-3 gap-4">
 
   
-FOOTER
+{/*footer 1 */}
+  <div className=" text-black">
+
+    <p className="text-2xl">LOGO HERE</p>
+
+    <p className="text-sm font-medium mt-8">Kemet Group Incorporated</p>
+
+    <p className="font-medium text-sm">EST 2023.</p>
+
+
+  </div>
+
+
+
+  <div className="text-xs font-light ">
+
+    <ul>
+        <li className="mt-2">Share our stories with your friends</li>
+        <li className="mt-2">Copyrights declaration</li>
+        <li className="mt-2">Modern slavery statement</li>
+
+        </ul>
+
+    
+</div>
+
+
+
+<div className="">
+
+    
+</div>
+
 </div>
         </>
 
