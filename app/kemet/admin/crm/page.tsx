c75958da3101528c582f3f1fd3a3446cadcdb64b
ebubelife <ebubeemeka19@gmail.com>
2023-11-23 10:05:37 +0100

    ...

--- app/kemet/admin/crm/page.tsx
+++ app/kemet/admin/crm/page.tsx
@@ -219,7 +219,9 @@ useEffect(() => {
         <div className="w-full h-32 shadow-xl rounded-xl border border-zinc-200 border-2 p-4">
   <p className="text-sm text-zinc-500 font-medium">Total Patient Requests</p>
 
-  // @ts-ignore
+
+  { // @ts-ignore
+}
   <p className="text-xl text-zinc-700 font-medium mt-4">{crmRequestData?.patients.length}</p>
 
   <div className="flex-shrink-0 align-items-flex-end text-sm">
@@ -230,7 +232,8 @@ useEffect(() => {
 <div className="w-full h-32 shadow-xl rounded-xl border border-zinc-200 border-2 p-4">
   <p className="text-sm text-zinc-500 font-medium">Total Prescriber Requests</p>
 
-  // @ts-ignore
+{ // @ts-ignore
+} 
   <p className="text-xl text-zinc-700 font-medium mt-4">{crmRequestData?.prescribers.length}</p>
 
   <div className="flex-shrink-0 align-items-flex-end text-sm">
@@ -240,7 +243,9 @@ useEffect(() => {
 
             <div className="w-full h-32 shadow-xl rounded-xl border border-zinc-200 border-2 p-4">
                  <p className="text-sm text-zinc-500 font-medium">Pharmacies</p>
-                 // @ts-ignore
+                
+{ // @ts-ignore
+}
               <p className="text-xl text-zinc-700 font-medium mt-4">{crmRequestData?.pharmacies.length}</p>
 
                 <div className="flex-shrink-0 align-items-flex-end text-sm">
